4795a9c8bbe0b7d2d5d67fe1cc59309eee15b1d5
DavidPalacio99 <dpalacioh@unal.edu.co>
2023-02-16 11:23:38 -0500

    Configuring openAI API

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "../styles/globals.css";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../pages/api/auth/[...nextauth]";
 import Login from "../Components/Login";
+import ClientProvider from "../Components/ClientProvider";
 
 export default async function RootLayout({
   children,
@@ -25,6 +26,7 @@ export default async function RootLayout({
               </div>
 
               {/* Client Provider - Notifications */}
+              <ClientProvider />
 
               <div className="bg-[#343541]  flex-1 ">{children}</div>
             </div>
