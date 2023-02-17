30e3e28dd1e9640d81dafbdf6f5b8ea91489d596
DavidPalacio99 <dpalacioh@unal.edu.co>
2023-02-17 12:16:59 -0500

    Adding no messages screen and making chat component scrollable

--- Components/Chat.tsx
+++ Components/Chat.tsx
@@ -7,6 +7,7 @@ import { collection, orderBy } from "firebase/firestore";
 import { db } from "../firebase";
 import { query } from "firebase/firestore";
 import Message from "./Message";
+import { ArrowDownCircleIcon } from "@heroicons/react/24/solid";
 
 type Props = {
   chatId: string;
@@ -31,7 +32,15 @@ const Chat = ({ chatId }: Props) => {
   );
 
   return (
-    <div className="flex-1">
+    <div className="flex-1 overflow-y-auto overflow-x-hidden ">
+      {messages?.empty && (
+        <>
+          <p className="mt-10 text-center text-white">
+            Type a prompt in below to get started!
+          </p>
+          <ArrowDownCircleIcon className="h-10 w-10 mx-auto mt-5 text-white animate-bounce" />
+        </>
+      )}
       {messages?.docs.map((message) => {
         return <Message key={message.id} message={message.data()} />;
       })}
